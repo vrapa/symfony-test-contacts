fddf1257bfbc5ed68d021f96e79aa5340c5b0a44
Radovan Kraus <koder@rkcomputer.cz>
2024-01-24 10:29:20 +0100

    úpravy composeru, vytvoření entity, databáze a  tabulky

--- README.md
+++ README.md
@@ -1,5 +1,20 @@
 # symfony-test-contacts
 Test app Contacts
+=================
+Instalace aplikace
+------------------
+1. Naklonovat repozitář
+2. Nainstalovat závislosti pomocí composeru
+3. Upravit .htaccess v kořenovém adresáři aplikace dle potřeby
+4. Upravit připojení k databázi v souboru .env
+5. Vytvořit databázi pomocí příkazu `php bin/console doctrine:database:create`
+6. Vytvořit tabulky pomocí příkazu `php bin/console doctrine:schema:update --force`
+6. Vytvořit repository pomocí příkazu `php bin/console doctrine:migrations:diff`
+
+
+
+
+
 
 Testovací URL:
 http://stc.local/
---
+++ src/Entity/Contacts.php
@@ -0,0 +1,127 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Table(name: 'contacts')]
+#[ORM\Entity(repositoryClass: 'App\Model\repositories\ContactsRepository')]
+class Contacts
+{
+    #[ORM\Column(name: 'id', type: 'integer', nullable: false)]
+    #[ORM\Id]
+    #[ORM\GeneratedValue(strategy: 'IDENTITY')]
+    private int $id;
+
+    #[ORM\Column(name: 'name', type: 'string', length: 50, nullable: false)]
+    private string $name;
+
+    #[ORM\Column(name: 'surname', type: 'string', length: 50, nullable: false)]
+    private string $surname;
+
+    #[ORM\Column(name: 'telefon', type: 'string', length: 20, nullable: true)]
+    private ?string $telefon;
+
+    #[ORM\Column(name: 'email', type: 'string', length: 50, nullable: false)]
+    private string $email;
+
+    #[ORM\Column(name: 'note', type: 'string', length: 255, nullable: true)]
+    private ?string $note;
+
+    /**
+     * @return int
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param int $id
+     */
+    public function setId(int $id): void
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $name
+     */
+    public function setName(string $name): void
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return string
+     */
+    public function getSurname(): string
+    {
+        return $this->surname;
+    }
+
+    /**
+     * @param string $surname
+     */
+    public function setSurname(string $surname): void
+    {
+        $this->surname = $surname;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getTelefon(): ?string
+    {
+        return $this->telefon;
+    }
+
+    /**
+     * @param string|null $telefon
+     */
+    public function setTelefon(?string $telefon): void
+    {
+        $this->telefon = $telefon;
+    }
+
+    /**
+     * @return string
+     */
+    public function getEmail(): string
+    {
+        return $this->email;
+    }
+
+    /**
+     * @param string $email
+     */
+    public function setEmail(string $email): void
+    {
+        $this->email = $email;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getNote(): ?string
+    {
+        return $this->note;
+    }
+
+    /**
+     * @param string|null $note
+     */
+    public function setNote(?string $note): void
+    {
+        $this->note = $note;
+    }
+
+}
