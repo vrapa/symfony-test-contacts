0c7cad37f8a22cabb3e0668284928eff0912bf5f
Radovan Kraus <koder@rkcomputer.cz>
2024-01-24 13:25:06 +0100

    Doplnění optimalizované SEO url

--- src/Controller/ContactsController.php
+++ src/Controller/ContactsController.php
@@ -9,6 +9,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\String\Slugger\SluggerInterface;
 
 // ...
 
@@ -23,7 +24,7 @@ class ContactsController extends AbstractController
     }
 
     #[Route('/')]
-    public function contactList(Request $request, PaginatorInterface $paginator): Response
+    public function contactList(Request $request, PaginatorInterface $paginator, SluggerInterface $slugger): Response
     {
         $queryBuilder = $this->contactsRepository->createQueryBuilder('c');
         $pagination = $paginator->paginate(
@@ -34,6 +35,28 @@ class ContactsController extends AbstractController
 
         return $this->render('contacts/list.html.twig', [
             'pagination' => $pagination,
+            'slugger' => $slugger,
         ]);
     }
+
+    #[Route('/{id}-{slug}', name: 'contact_edit', requirements: ['id' => '\d+', 'slug' => '[a-z0-9-]+'])]
+    public function contactEdit(int $id, string $slug, SluggerInterface $slugger): Response
+    {
+        $contact = $this->contactsRepository->find($id);
+
+        // Kontrola, zda slug odpovídá kontaktu
+        $expectedSlug = $slugger->slug($contact->getName())->lower();
+
+        if ($slug !== (string)$expectedSlug) {
+            return $this->redirectToRoute('contact_edit', [
+                'id' => $id,
+                'slug' => $expectedSlug,
+            ]);
+        }
+
+        return $this->render('contacts/edit.html.twig', [
+        ]);
+    }
+
+
 }
