318875518d1babc4056de973c882ed8581491d2d
Radovan Kraus <koder@rkcomputer.cz>
2024-01-25 10:42:01 +0100

    fix phpstan

--- src/Form/ContactFormType.php
+++ src/Form/ContactFormType.php
@@ -13,7 +13,7 @@ use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 
 class ContactFormType extends AbstractType
 {
-    public function buildForm(FormBuilderInterface $builder, array $options)
+    public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
             ->add('name', TextType::class, [
@@ -44,7 +44,7 @@ class ContactFormType extends AbstractType
             ]);
     }
 
-    public function configureOptions(OptionsResolver $resolver)
+    public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([
             'data_class' => ContactData::class,
